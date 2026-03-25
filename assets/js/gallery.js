a3c736cc6d1f2d6a9d82b9c1ed3bd80edbefb54a
mohammad asad <asadkhan41376@gmail.com>
2026-03-25 15:00:40 +0530

    some page add

---
+++ assets/js/gallery.js
@@ -0,0 +1,14 @@
+
+
+      // LIGHTBOX OPEN
+      function openLightbox(src) {
+        document.getElementById("imageLightbox").style.display = "flex";
+        document.getElementById("lightboxImg").src = src;
+      }
+
+      // LIGHTBOX CLOSE
+      function closeLightbox() {
+        document.getElementById("imageLightbox").style.display = "none";
+      }
+
+     
